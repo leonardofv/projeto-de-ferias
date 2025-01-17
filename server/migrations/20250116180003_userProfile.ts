d86b7ebaae23580f6503355a5ff17247e899c1f5
Leonardo Vasconcelos <fonteles.leonardo@mail.com>
2025-01-16 21:21:54 -0300

    update user_profile relationship

--- server/migrations/20250116180003_userProfile.ts
+++ server/migrations/20250116180003_userProfile.ts
@@ -2,16 +2,15 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.withSchema('public').createTable('userProfile', (table) => {
-        table.increments();
-        table.integer('userId').references('id').inTable('users').notNullable().unique().onDelete('CASCADE');
-        table.string('name').notNullable();
-        table.string('profilePicture');
+    return knex.schema.withSchema('public').createTable('user_profile', (table) => {
+        table.integer('user_id').references('id').inTable('users').primary().onDelete('CASCADE');
+        table.string('name')
+        table.string('profile_picture');
         table.string('bio');
     });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.withSchema('public').dropTable('userProfile');
+    return knex.schema.withSchema('public').dropTable('user_profile');
 }
