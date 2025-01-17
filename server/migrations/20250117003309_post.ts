442de701895ec376ca5f3c0d05a27eadf9514da4
Leonardo Vasconcelos <fonteles.leonardo@mail.com>
2025-01-16 22:08:32 -0300

    remove primary in table post

--- server/migrations/20250117003309_post.ts
+++ server/migrations/20250117003309_post.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.withSchema('public').createTable('post', (table) => {
         table.increments();
-        table.integer('user_id').references('id').inTable('users').primary().onDelete('CASCADE');
+        table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
         table.string('path').notNullable();
         table.date('publish_date').notNullable();
         table.string('description');
