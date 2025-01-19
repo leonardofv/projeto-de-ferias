f0c9b7285095a93f6853bc15e4ce6ba468d2b3d0
Leonardo Vasconcelos <fonteles.leonardo@mail.com>
2025-01-18 22:51:49 -0300

    add post endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,9 @@ app.get('/', (_, res) => {
   res.status(200).json({ message: 'Hello, World 🗺️!' });
 });
 
+//register
 app.post('/auth/register', async (req, res) => {
+
   const { email, password, password2, username } = req.body;
 
   if (password !== password2) {
@@ -39,6 +41,57 @@ app.post('/auth/register', async (req, res) => {
     res.status(500).json({ message: 'Something went wrong 😢❌' });
   }
 });
+
+//login
+app.post('/auth/login', async (req, res) => {
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({message: 'email and password are required'});
+    return;
+  }
+
+  try {
+    const [user] = await db('users').where({email});
+
+    if(user.password != password) {
+      res.status(400).json({message: 'Invalid email or password'});
+      return;
+    }
+
+    res.status(201).json({message: 'Login Successful✅', data: user})
+
+  }catch(err) {
+    console.error(err);
+    res.status(500).json({ message: 'Something went wrong 😢❌' });
+  }
+
+})
+
+
+//post
+app.post('/post', async (req, res) => {
+  const { user_id, path, publish_date, description } = req.body;
+
+  if(!user_id || !path) {
+    res.status(401).json({message: 'user_id and path are required'});
+    return;
+  }
+    try{
+      const [post] = await db
+      .insert({user_id, path, publish_date: publish_date ?? new Date(), description})
+      .into('post')
+      .returning(['user_id', 'path', 'publish_date', 'description']);
+
+      res.status(201).json({message: 'post created successful✅', data: post});
+
+    }catch(err){
+      console.error(err);
+      res.status(500).json({ message: 'Something went wrong 😢❌' });
+    }
+  })
+
 
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`),
